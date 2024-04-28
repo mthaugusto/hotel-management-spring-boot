5dd0b3863a367bef527bd2c6eaf540717bd8f778
mthaugusto <mthaugusto94@icloud.com>
2024-04-28 08:08:56 -0300

    Adicionando a lógica do findAll com parâmetros

--- src/main/java/br/com/fiap/hotelmanagement/resource/HotelResource.java
+++ src/main/java/br/com/fiap/hotelmanagement/resource/HotelResource.java
@@ -2,8 +2,11 @@ package br.com.fiap.hotelmanagement.resource;
 
 import br.com.fiap.hotelmanagement.dto.request.HotelRequest;
 import br.com.fiap.hotelmanagement.dto.response.HotelResponse;
+import br.com.fiap.hotelmanagement.entity.Hotel;
 import br.com.fiap.hotelmanagement.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,13 +19,41 @@ public class HotelResource {
     @Autowired
     private HotelService service;
 
+//    @GetMapping
+//    public List<HotelResponse> findAll() {
+//        var encontrados = service.findAll();
+//        var resposta = encontrados.stream()
+//                .map(service::toResponse)
+//                .toList();
+//        return resposta;
+//    }
+
     @GetMapping
-    public List<HotelResponse> findAll() {
-        var encontrados = service.findAll();
+    public List<HotelResponse> findAll(
+            @RequestParam(name = "nome", required = false) String nome
+    ) {
+
+        var hotel = Hotel.builder()
+                .nome(nome)
+                .build();
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matchingAll()
+                .withIgnoreNullValues()
+                .withIgnoreCase();
+
+        Example<Hotel> example = Example.of(hotel, matcher);
+
+        var encontrados = service.findAll(example);
+
+
         var resposta = encontrados.stream()
                 .map(service::toResponse)
                 .toList();
+
         return resposta;
+
+
     }
 
     @GetMapping(value="/{id}")
